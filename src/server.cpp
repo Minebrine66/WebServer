0aa3d3cc3fea90b7f0112c7e419d153cc713138d
Derek Lew <minebrinecrazy@gmail.com>
2025-03-29 15:20:50 -0500

    added basic error handling for arguments. added base supporting files

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include <iostream>
-
+#include <stdexcept>
 
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -11,7 +11,19 @@ using std::endl;
 
 int main(int argc, char* argv[])
 {
-    
+    try 
+    {
+        if(argc != 2) 
+        {
+            throw std::runtime_error(
+                "Invalid arguments.\nusage: ./server <port_num>");
+        }
+    }
+    catch (const std::exception& e)
+    {
+        cout << "Exception " << e.what() << endl;
+        return -1;
+    }
     cout << "test" << endl;
     return 0;
 }
