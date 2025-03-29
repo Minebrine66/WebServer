cf24e30ea6cb782bf76c595015438954429db625
Derek Lew <minebrinecrazy@gmail.com>
2025-03-29 16:22:20 -0500

    rearranged argument size error handling

--- src/server.cpp
+++ src/server.cpp
@@ -5,21 +5,31 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
+using std::cout;
+using std::endl;
+using std::cerr;
+
+void Argument_Size_Test(int argc)
+{
+    if(argc != 2) 
+    {
+        throw std::runtime_error(
+            "Invalid arguments.\nusage: ./server <port_num>");
+    }
+}
+
 int main(int argc, char* argv[])
 {
+    //checks if port number has been specified
     try 
     {
-        if(argc != 2) 
-        {
-            throw std::runtime_error(
-                "Invalid arguments.\nusage: ./server <port_num>");
-        }
+        Argument_Size_Test(argc);
     }
     catch (const std::exception& e)
     {
-        std::cout << "Exception " << e.what() << std::endl;
+        cerr << "Exception " << e.what() << endl;
         return -1;
     }
-    std::cout << "test" << std::endl;
+    cout << "test" << endl;
     return 0;
 }
