2e7b0e422e7ec6565e418d9a29c663ccfd054253
Derek Lew <minebrinecrazy@gmail.com>
2025-04-12 16:51:00 -0500

    removed exception handling, using basic error handling

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <stdexcept>
+#include <string>
 
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -9,27 +10,37 @@ using std::cout;
 using std::endl;
 using std::cerr;
 
-void Argument_Size_Test(int argc)
+uint16_t strToInt(char argv[])
 {
-    if(argc != 2) 
+    //char *last;
+    //converts to number in base 10
+    long num = std::stoi(argv, nullptr, 10);
+    //performs check if number is malformed or out of bounds
+    cout << num << endl;
+    if (num < 0 || num > UINT16_MAX)
     {
-        throw std::runtime_error(
-            "Invalid arguments.\nusage: ./server <port_num>");
+        return 0;
     }
+    return num;
 }
 
 int main(int argc, char* argv[])
 {
     //checks if port number has been specified
-    try 
+    if(argc != 2)
     {
-        Argument_Size_Test(argc);
-    }
-    catch (const std::exception& e)
-    {
-        cerr << "Exception " << e.what() << endl;
+        cerr << "Invalid arguments!\nusage: ./server <port_num>" << endl;
         return -1;
     }
+    //converts provided argument into int
+    uint16_t port = strToInt(argv[1]);
+    if (port == 0)
+    {
+        cerr << "Invalid port number!" << endl;
+        return -1;
+    }
+
+
     cout << "test" << endl;
     return 0;
 }
