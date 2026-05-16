5f25bded5181bd9c4392243ea750d6706e54b83c
Derek Lew <minebrinecrazy@gmail.com>
2026-05-16 15:00:52 -0700

    fixed typos

--- src/server.cpp
+++ src/server.cpp
@@ -11,16 +11,18 @@ using std::cout;
 using std::endl;
 using std::cerr;
 
+#define UINT16_MAX 65535
+
 // Converts String to Int
 uint16_t strToInt(char argv[])
 {
-    //checks if argument has all numerical chars
+    // checks if argument has all numerical chars
     std::string arg(argv);
     if (arg.find_first_not_of("0123456789") == std::string::npos)
     {
-        //converts to number in base 10
+        // converts to number in base 10
         long num = std::stoi(argv);
-        //performs check if number is malformed or out of bounds
+        // performs check if number is malformed or out of bounds
         if (num < 0 || num > UINT16_MAX)
         {
             return 0;
@@ -40,27 +42,30 @@ int create_socket(uint16_t port)
 
     sockaddr_in serverIP;
     serverIP.sin_family = AF_INET;
-    serverIP.sin_port = hton(port);
-    serverIP.sin_Addr.s_addr = INADDR_ANY;
+    serverIP.sin_port = htons(port);
+    serverIP.sin_addr.s_addr = INADDR_ANY;
 
     //bind socket
     if (bind(serverSocket, (struct sockaddr*)&serverIP, sizeof(serverIP)) < 0)
     {
-        
+        // failed to bind, error out 
+        return -1;
     }
+    
     
 
 }
+
 int main(int argc, char* argv[])
 {
-    //checks if port number has been specified
+    // checks if port number has been specified
     if(argc != 2)
     {
         cerr << "ERROR: Invalid arguments!\nusage: ./server <port_num>" << endl;
         return -1;
     }
     uint16_t port = strToInt(argv[1]);
-    //checks if port number is valid
+    // checks if port number is valid
     if (port == 0)
     {
         cerr << "ERROR: Invalid port number!" << endl;
